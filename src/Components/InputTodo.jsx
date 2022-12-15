d12019b65f94c5455ecb1bc05c5f7ed721c8022d
shalash23 <shalashmohamed232@outlook.com>
2022-12-15 16:04:59 +0200

    last commit at work

--- src/Components/InputTodo.jsx
+++ src/Components/InputTodo.jsx
@@ -2,6 +2,28 @@ import React from "react";
 import { Box, Paper, TextField } from "@mui/material";
 import InputCheckbox from "./InputCheckbox";
 const InputTodo = () => {
+  const [data, setData] = React.useState({
+    task: "",
+  });
+  const handleChange = (e) => {
+    setData((prevData) => ({
+      task: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    const response = await fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      body: JSON.stringify({
+        data,
+      }),
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    });
+  };
+
   return (
     <Paper
       elevation={2}
@@ -25,6 +47,8 @@ const InputTodo = () => {
             paddingY: 1.5,
           }}
           placeholder="Create a new todo"
+          onChange={handleChange}
+          onSubmit={handleSubmit}
         />
       </Box>
     </Paper>
